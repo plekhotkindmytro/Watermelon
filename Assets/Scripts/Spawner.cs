3fedafef38567a8dec527e5ecf3dadb815faaa71
Dmytro <plekhotkindmytro@users.noreply.github.com>
2024-10-29 12:20:21 +0200

    Update next feaure

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 {
+    public Image nextFruitImage;
     public List<GameObject> fruitPrefabs;  // List of all 11 fruit prefabs
 
-
     private Fruit controlledFruit = null;
-    private bool dropped = false;
+    private Fruit nextFruit = null;
+    
     private Camera cameraMain;
+
 
     public void Start()
     {
@@ -44,10 +47,27 @@ public class Spawner : MonoBehaviour
     private void SpawnFruit()
     {
         // Randomly select a fruit prefab and instantiate it at the spawner's position
-        int randomIndex = Random.Range(0, GameManager.Instance.maxFruitLevel);
-        GameObject fruitGameObject = Instantiate(fruitPrefabs[randomIndex], transform.position, Quaternion.identity);
-        controlledFruit = fruitGameObject.GetComponent<Fruit>();
+        int randomIndex;
 
+        if(nextFruit == null)
+        {
+            randomIndex = Random.Range(0, GameManager.Instance.maxFruitLevel);
+            GameObject fruitGameObject = Instantiate(fruitPrefabs[randomIndex], transform.position, Quaternion.identity);
+
+            controlledFruit = fruitGameObject.GetComponent<Fruit>();
+        } else
+        {
+            controlledFruit = nextFruit;
+            controlledFruit.gameObject.SetActive(true);
+        }
         
+
+        // next
+        randomIndex = Random.Range(0, GameManager.Instance.maxFruitLevel);
+        GameObject nextGameObject = Instantiate(fruitPrefabs[randomIndex], transform.position, Quaternion.identity);
+        nextGameObject.SetActive(false);
+        nextFruit = nextGameObject.GetComponent<Fruit>();
+        nextFruitImage.sprite = nextGameObject.GetComponent<SpriteRenderer>().sprite;
+
     }
 } 
