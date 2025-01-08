b96cd0825b8f0e8f693e4ddf047c49b8f8e901ab
Dmytro <plekhotkindmytro@users.noreply.github.com>
2025-01-09 00:06:32 +0200

    Add Google Play Services library

---
+++ Assets/GooglePlayManager.cs
@@ -0,0 +1,23 @@
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class GooglePlayManager : MonoBehaviour
+{
+    void Start() 
+    {
+      //  PlayGamesPlatform.Activate();
+
+        Social.localUser.Authenticate(success =>
+        {
+            if (success)
+            {
+                Debug.Log("Player authenticated.");
+            }
+            else
+            {
+                Debug.LogError("Failed to authenticate player.");
+            }
+        });
+    }
+}
